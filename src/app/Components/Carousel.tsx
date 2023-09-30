336b7ae482b18b0f9b3f4981f522bdf236341122
RoshanDalami <roshandalami0@gmail.com>
2023-10-01 01:35:52 +0545

    responsive crousel

--- src/app/Components/Carousel.tsx
+++ src/app/Components/Carousel.tsx
@@ -33,7 +33,34 @@ export default function SliderComp({children}:PropsWithChildren){
         slidesToShow: 3,
         slidesToScroll: 1,
         nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    
       };
 
       return(
